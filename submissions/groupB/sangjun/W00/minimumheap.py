b06f988aa18e6d66fa1700b8fda3a77dbcc963c4
yeosangjun824-gif <yeosangjun824@gmail.com>
2026-01-25 22:49:03 +0900

    test: add W00 test

--- submissions/groupB/sangjun/W00/minimumheap.py
+++ submissions/groupB/sangjun/W00/minimumheap.py
@@ -1,8 +1,7 @@
 import sys
 n = int(sys.stdin.readline())
 heap =[]
-# x가 자연수이면 배열에 x를 추가 x가 0이면 배열에서 가장 작은값 출력 후 배열에서 제거
-#만약 배열이 비어있는 경우에 가장 작은 값을 출력하라는 경우 0을 출력한다.
+
 for i in range(n):
     x = int(sys.stdin.readline())
     if x == 0:
